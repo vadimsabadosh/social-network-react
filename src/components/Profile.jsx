d1babf76fb3f12dd24ba58654717841053ae171a
vadimsabadosh <vadimsabadosh@gmail.com>
2021-03-22 15:50:38 +0200

    App initialized

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+const Profile = () => {
+  return(
+    <main className='main'>
+    <div className='main-img'>
+      <img src='' />
+    </div>
+    <div className='profile-content'>
+      ava+ desc
+    </div>
+    <div className='posts-wrapper'>
+      <div>My Posts</div>
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          post
+        </div>
+      </div>
+    </div>
+  </main>
+  )
+}
+export default Profile;
