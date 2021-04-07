fc9b6f3dfa8a36b8377d32b3e2e31f5cd358b0de
vadimsabadosh <vadimsabadosh@gmail.com>
2021-04-07 20:47:13 +0300

    94 React Lazy & React Suspense

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './app.css';
 import store from './redux/redux-store';
 import NavBar from './components/NavBar/NavBar';
-import UsersContainer from './components/Users/UsersContainer';
 import { BrowserRouter, Route } from 'react-router-dom';
 import MessagesContainer from './components/Messages/MessagesContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import { connect, Provider } from 'react-redux';
 import { setInitApp } from './redux/reducerApp';
 import Preloader from './components/common/Preloader/Preloader';
+
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'))
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'))
 
 class App extends React.Component {
 
@@ -34,13 +35,23 @@ class App extends React.Component {
 
             <Route 
               path='/profile/:userId?' 
-              render={() => <ProfileContainer />
+              render={() => {
+                return <Suspense fallback={<div>Loading</div>}>
+                  <ProfileContainer />
+                </Suspense>
+              }
               }
             />
             <Route 
               exact 
               path='/users' 
-              render={() => <UsersContainer />}
+              render={() => {
+                return <Suspense fallback={<div>Loading</div>}>
+                  <UsersContainer />
+                </Suspense>
+                }
+              }
+              
             />
             <Route 
               exact 
